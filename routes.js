8a407d54a05159e87e5cc8cd17aa4a991988c704
dkearns-lumination <dkearns@lumination.com.au>
2023-05-25 09:45:17 +0930

    Add rate limiting and hide some headers

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
     //Hosting the launcher
     app.get('/program-launcher-version', (req, res) => {
-        res.send(`1.0.1 ${baseURL(req)} 8e619a0ea8c776bff9d8bb3c34a68bed11ff6e65`);
+        res.send(`1.0.1 ${baseURL(req.hostname, 'program-launcher')} 8e619a0ea8c776bff9d8bb3c34a68bed11ff6e65`);
     });
 
     app.get('/program-launcher', (req, res) => {
@@ -11,7 +11,7 @@ module.exports = (app) => {
 };
 
 //Get the base URL of the current hosting site and return the program route (the queried minus the version string)
-let baseURL = (req) => {
-    let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+let baseURL = (host, path) => {
+    let fullUrl = 'https://' + host + "/" + path;
     return baseUrl = fullUrl.replace("-version", "");
 }
